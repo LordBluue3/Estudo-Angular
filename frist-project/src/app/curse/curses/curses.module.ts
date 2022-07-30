82a930373e806b041eefeb4d496655516f6f8415
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-07-30 16:23:20 -0300

    Criando novo modulo

---
+++ frist-project/src/app/curse/curses/curses.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetailCurseComponent } from './detail-curse/detail-curse.component';
+
+
+
+@NgModule({
+  declarations: [
+    DetailCurseComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CursesModule { }
